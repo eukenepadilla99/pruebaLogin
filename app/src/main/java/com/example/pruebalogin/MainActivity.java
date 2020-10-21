528a35c415108cd2fb79fe34d505aadfdb14ae8e
eukenepadilla99 <eukenepadilla@gmail.com>
2020-10-21 06:51:07 +0200

    intentos de login sin resultado positivo

---
+++ app/src/main/java/com/example/pruebalogin/MainActivity.java
@@ -0,0 +1,170 @@
+package com.example.pruebalogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.quickstart.auth.databinding.ActivityEmailpasswordBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 12345 ;
+    GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+    private ActivityEmailpasswordBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("miFiltro", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//        updateUI(account);
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d("miFiltro", "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressBar();
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressBar();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mBinding.fieldEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mBinding.fieldEmail.setError("Required.");
+            valid = false;
+        } else {
+            mBinding.fieldEmail.setError(null);
+        }
+
+        String password = mBinding.fieldPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mBinding.fieldPassword.setError("Required.");
+            valid = false;
+        } else {
+            mBinding.fieldPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressBar();
+        if (user != null) {
+            mBinding.status.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            mBinding.detail.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            mBinding.emailPasswordButtons.setVisibility(View.GONE);
+            mBinding.emailPasswordFields.setVisibility(View.GONE);
+            mBinding.signedInButtons.setVisibility(View.VISIBLE);
+
+            if (user.isEmailVerified()) {
+                mBinding.verifyEmailButton.setVisibility(View.GONE);
+            } else {
+                mBinding.verifyEmailButton.setVisibility(View.VISIBLE);
+            }
+        } else {
+            mBinding.status.setText(R.string.signed_out);
+            mBinding.detail.setText(null);
+
+            mBinding.emailPasswordButtons.setVisibility(View.VISIBLE);
+            mBinding.emailPasswordFields.setVisibility(View.VISIBLE);
+            mBinding.signedInButtons.setVisibility(View.GONE);
+        }
+    }
+}
